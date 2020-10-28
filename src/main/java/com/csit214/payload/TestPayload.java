e4beabeaaf9636d59b0a9a03e7089837075198c0
loc le <tienloc.le2604@gmail.com>
2020-10-28 07:45:44 +0700

    commit work

---
+++ src/main/java/com/csit214/payload/TestPayload.java
@@ -0,0 +1,137 @@
+package com.csit214.payload;
+
+import java.util.Objects;
+
+public class TestPayload {
+    private String a;
+    private  String b;
+    private String c;
+    private String d;
+    private String e;
+    private String f;
+    private String g;
+    private String h;
+    private String i;
+
+    public TestPayload(String a, String b, String c, String d, String e, String f, String g, String h, String i) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.g = g;
+        this.h = h;
+        this.i = i;
+    }
+
+    @Override
+    public String toString() {
+        return "TestPayload{" +
+                "a='" + a + '\'' +
+                ", b='" + b + '\'' +
+                ", c='" + c + '\'' +
+                ", d='" + d + '\'' +
+                ", e='" + e + '\'' +
+                ", f='" + f + '\'' +
+                ", g='" + g + '\'' +
+                ", h='" + h + '\'' +
+                ", i='" + i + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        TestPayload that = (TestPayload) o;
+        return Objects.equals(a, that.a) &&
+                Objects.equals(b, that.b) &&
+                Objects.equals(c, that.c) &&
+                Objects.equals(d, that.d) &&
+                Objects.equals(e, that.e) &&
+                Objects.equals(f, that.f) &&
+                Objects.equals(g, that.g) &&
+                Objects.equals(h, that.h) &&
+                Objects.equals(i, that.i);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c, d, e, f, g, h, i);
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+
+    public String getF() {
+        return f;
+    }
+
+    public void setF(String f) {
+        this.f = f;
+    }
+
+    public String getG() {
+        return g;
+    }
+
+    public void setG(String g) {
+        this.g = g;
+    }
+
+    public String getH() {
+        return h;
+    }
+
+    public void setH(String h) {
+        this.h = h;
+    }
+
+    public String getI() {
+        return i;
+    }
+
+    public void setI(String i) {
+        this.i = i;
+    }
+}
