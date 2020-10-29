8924a2f99d3e2bd9d588f3f8b4f0f02552a43d76
loc le <tienloc.le2604@gmail.com>
2020-10-29 14:32:57 +0700

    email validator

---
+++ src/main/java/com/csit214/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.csit214.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "FlyDreamAir Server is successfully initiated";
+    }
+}
