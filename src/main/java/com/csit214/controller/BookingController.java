bb190b392d746fca4349e85d90fc56f757f5fa96
loc le <tienloc.le2604@gmail.com>
2020-11-05 19:33:40 +0700

    set voucher validity

--- src/main/java/com/csit214/controller/BookingController.java
+++ src/main/java/com/csit214/controller/BookingController.java
@@ -47,7 +47,7 @@ public class BookingController {
             return new ResponseEntity(new ApiResponse(false, "Voucher code is invalid!"),
                     HttpStatus.valueOf(200));
         }
-        if(!voucher.isValid()){
+        if( voucher != null && !voucher.isValid()){
             return new ResponseEntity(new ApiResponse(false, "Voucher code has been used!"),
                     HttpStatus.valueOf(200));
         }
